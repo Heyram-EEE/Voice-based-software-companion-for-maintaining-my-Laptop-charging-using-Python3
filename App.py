30957e0db9a8b3d4a954fb3efc50a95547081b22
Heyram EEE <62865055+Heyram-EEE@users.noreply.github.com>
2023-06-07 21:30:12 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,28 @@
+import pyttsx3
+import psutil
+import time
+
+def say(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+stat=None
+print("LapLiMate - A Laptop Battery Companian")
+print("Powered by He5 Solutions he5.in")
+print("Running...............")
+while(True):
+    battery = psutil.sensors_battery()
+    cstat= battery.power_plugged
+    percent=battery.percent
+    if((stat==False)and(cstat==True)):
+        say("Thanks for Connecting the Charger")
+    if(stat!=cstat):
+        stat=cstat
+    if((percent>85)and(cstat==True)):
+        say("Please disconnect the charger Battery is "+str(percent))
+        time.sleep(30)
+    elif((percent<20)and(cstat==False)):
+        say("Connect the charger Battery percentage is "+str(percent))
+        time.sleep(30)
+    time.sleep(1)
